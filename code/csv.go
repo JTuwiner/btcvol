2424d2eee40f7658aa764a4683bfc326ecdd8290
Eli Dourado <elidourado@gmail.com>
2015-08-27 09:39:01 -0400

    Fix CSV output
    Add additional value placeholder for CSV

--- code/csv.go
+++ code/csv.go
@@ -18,6 +18,6 @@ func csvHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", "attachment;filename=btcvol.csv")
 	fmt.Fprintln(w, "date,price,logprice,return,volatility,volatility60")
 	for _, v := range d.Data[29:] {
-		fmt.Fprintf(w, "%v,%v,%v,%v,%v\n", v.Date, v.Price, v.Logprice, v.Return, v.Volatility/100.0, v.Volatility60/100.0)
+		fmt.Fprintf(w, "%v,%v,%v,%v,%v,%v\n", v.Date, v.Price, v.Logprice, v.Return, v.Volatility/100.0, v.Volatility60/100.0)
 	}
 }
