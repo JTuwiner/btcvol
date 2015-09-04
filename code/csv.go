6a4629322aa71c483bd02fcde6e86ff4234b8b76
Eli Dourado <elidourado@gmail.com>
2015-09-03 22:19:31 -0400

    Convert leading zeroes in vol60 to null

--- code/csv.go
+++ code/csv.go
@@ -18,6 +18,14 @@ func csvHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", "attachment;filename=btcvol.csv")
 	fmt.Fprintln(w, "date,price,logprice,return,volatility,volatility60")
 	for _, v := range d.Data[29:] {
-		fmt.Fprintf(w, "%v,%v,%v,%v,%v,%v\n", v.Date, v.Price, v.Logprice, v.Return, v.Volatility/100.0, v.Volatility60/100.0)
+		fmt.Fprintf(w, "%v,%v,%v,%v,%v,%v\n", v.Date, v.Price, v.Logprice, v.Return, v.Volatility/100.0, zerotonull(v.Volatility60/100.0))
+	}
+}
+
+func zerotonull(n float64) (string) {
+	if (n==0) {
+		return ""
+	} else {
+		return fmt.Sprintf("%v",n)
 	}
 }
