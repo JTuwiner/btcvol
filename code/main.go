8aad933784fef3de84ae3dba8d061547d8f28d91
Eli Dourado <elidourado@gmail.com>
2016-12-30 16:04:10 -0500

    Add debugging pages

--- code/main.go
+++ code/main.go
@@ -15,6 +15,8 @@ type RenderData struct {
 func init() {
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/update", updateHandler)
+	http.HandleFunc("/debug", debugHandler)
+	http.HandleFunc("/debuge", debugeHandler)
 	http.HandleFunc("/all", allHandler)
 	http.HandleFunc("/latest", latestHandler)
 	http.HandleFunc("/csv", csvHandler)
@@ -41,4 +43,25 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if err := t.Execute(w, renderdata); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func debugHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	now := time.Now()
+	u := "http://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-18&end=" + now.Format("2006-01-02")
+	body, err := fetch(u, c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Write(body)
+}
+
+func debugeHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := "https://etherchain.org/api/statistics/price"
+	body, err := fetch(u, c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Write(body)
 }
